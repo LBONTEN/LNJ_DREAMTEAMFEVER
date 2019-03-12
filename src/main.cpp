a2d2101b415f9ed9042b1360898d825b9a50b976
LENNYBONTENAKEL <lenny.bontenakel@student.uantwerpen>
2019-03-12 12:26:44 +0100

    started implementing input

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,45 @@
  */
 
 #include <iostream>
+#include <fstream>
+
+#include "./tinystuff/tinyxml.h"
 #include "RoadSystem.h"
+#include "Vehicle.h"
+#include "Road.h"
+
+bool ReadXML(std::string inputFileName)
+{
+    TiXmlDocument inputfile;
+
+    if(!inputfile.LoadFile(inputFileName.c_str()))
+    {
+        std::cerr << "InputFile not found" << endl;
+        return false;
+    }
+    TiXmlElement* root = inputfile.FirstChildElement();
+    if(root == NULL)
+    {
+        std::cerr << "Input invalid" << endl;
+        inputfile.Clear();
+        return false;
+    }
+    std::cout << root->Value() << endl;
+
+    TiXmlElement* element = root->NextSiblingElement();
+    for(TiXmlElement* elem = element; elem != NULL; elem = elem->NextSiblingElement())
+    {
+        std::cout << elem->Value() << endl;
+    }
+    return true;
+}
+
+
+
+
 
 int main() {
-    std::cout << "It's pizza time!" << std::endl;
+    ReadXML("single_connection_n_cars.xml");
 
-
-
-
-    return 69;
+    return 0;
 }
