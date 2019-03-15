fd131a507d4aeccd7598060a10f52ff514cd723d
Joren Van Borm <Joren.VanBorm@student.uantwerpen.be>
2019-03-15 15:28:59 +0100

    Edited build targets

---
+++ src/tests.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Joren Van Borm & Lenny Bontenakel
+//
+
+/// Tests for debug target go here ///
+
+#include "RoadSystem.h"
+#include "XMLParser.h"
+#include <iostream>
+
+int main() {
+    std::cout << "no tests to run just yet" << endl;
+}
