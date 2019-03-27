e17f32910cdc5e5c42be039e77608fc8e371b638
Joren Van Borm <Joren.VanBorm@student.uantwerpen.be>
2019-03-27 12:53:36 +0100

    added test files for Vehicle (& it's subclasses)

---
+++ src/VehicleTests.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Joren Van Borm on 2019-03-27.
+//
+
+#include "VehicleTests.h"
---
+++ src/VehicleTests.h
@@ -0,0 +1,12 @@
+//
+// Created by Joren Van Borm on 2019-03-27.
+//
+
+#ifndef LNJPSE_VEHICLETESTS_H
+#define LNJPSE_VEHICLETESTS_H
+
+
+
+
+
+#endif //LNJPSE_VEHICLETESTS_H
