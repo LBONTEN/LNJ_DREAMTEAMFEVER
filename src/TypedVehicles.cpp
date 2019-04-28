303fbb534401b5f93c757ac3fb702e50936f9f23
Joren Van Borm <Joren.VanBorm@student.uantwerpen.be>
2019-04-28 16:01:45 +0200

    added update function for bus (currently doesn't read out distance to bus stops)

--- src/TypedVehicles.cpp
+++ src/TypedVehicles.cpp
@@ -23,24 +23,125 @@ extern const VehicleLimits stdBusLimits(-7, 1, 0, 19);
 extern const unsigned int stdTruckLength = 15;
 extern const VehicleLimits stdTruckLimits(-6, 1, 0, 25);
 
+
+///--- MotorCycle ---///
+
+MotorCycle::MotorCycle() :
+        DefaultVehicle::DefaultVehicle()
+{
+    typeName = "MotorCycle";
+    
+    ENSURE(getTypeName() == "MotorCycle", "MotorCycle constructor failed to set typeName");
+}
+
+MotorCycle::MotorCycle(RoadSystem* environment, const string& licensePlate, Road* currentRoad) :
+        DefaultVehicle::DefaultVehicle(environment, licensePlate, currentRoad, "Car", stdCarLength, &stdCarLimits)
+{
+}
+
+MotorCycle::MotorCycle(RoadSystem* environment, const string& licensePlate, Road* currentRoad, int acceleration,
+                       int speed, unsigned int position) :
+        DefaultVehicle::DefaultVehicle(environment, licensePlate, currentRoad, acceleration, speed, position, "Car", stdCarLength, &stdCarLimits)
+{
+}
+
+
 ///--- Car ---///
 
-Car::Car() : DefaultVehicle::DefaultVehicle()
+Car::Car() :
+        DefaultVehicle::DefaultVehicle()
 {
-    typeName = "Car";
-
-    ENSURE(typeName == "Car", "Car constructor failed to set typeName");
 }
 
 Car::Car(RoadSystem* environment, const string& licensePlate, Road* currentRoad) :
         DefaultVehicle::DefaultVehicle(environment, licensePlate, currentRoad, "Car", stdCarLength, &stdCarLimits)
 {
-
 }
 
 Car::Car(RoadSystem* environment, const string& licensePlate, Road* currentRoad, int acceleration, int speed,
          unsigned int position) :
         DefaultVehicle::DefaultVehicle(environment, licensePlate, currentRoad, acceleration, speed, position, "Car", stdCarLength, &stdCarLimits)
 {
+}
 
+
+///--- Bus ---///
+
+Bus::Bus() :
+        DefaultVehicle::DefaultVehicle(),
+        busStopCooldown(30)
+{
+    typeName = "Bus";
+    
+    ENSURE(getTypeName() == "Bus", "Bus constructor failed to set typeName");
+}
+
+Bus::Bus(RoadSystem* environment, const string& licensePlate, Road* currentRoad) :
+        DefaultVehicle::DefaultVehicle(environment, licensePlate, currentRoad, "Car", stdCarLength, &stdCarLimits),
+        busStopCooldown(30)
+{
+}
+
+Bus::Bus(RoadSystem* environment, const string& licensePlate, Road* currentRoad, int acceleration,
+             int speed, unsigned int position) :
+        DefaultVehicle::DefaultVehicle(environment, licensePlate, currentRoad, acceleration, speed, position, "Car", stdCarLength, &stdCarLimits),
+        busStopCooldown(30)
+{
+}
+
+void Bus::execUpdate()
+{
+    unsigned int dToStop = std::numeric_limits<unsigned int>::max(); // TODO: set dToStop to actual distance
+    
+    // waiting at bus stop
+    if (dToStop == 0 and busStopCooldown > 0)
+    {
+        hardSetSpeed(0);
+        hardSetAcceleration(0);
+        
+        --busStopCooldown;
+    
+        return;
+    }
+    
+    // slowing down for bus stop
+    else if (dToStop <= 2 * (0.75*getSpeed()+getLen()) )
+    {
+        stepPosition();
+        stepSpeed();
+        
+        fullStop(dToStop);
+        
+        return;
+    }
+    
+    // default driving behavior
+    else
+    {
+        DefaultVehicle::execUpdate();
+    
+        return;
+    }
+}
+
+
+///--- Truck ---///
+
+Truck::Truck() :
+        DefaultVehicle::DefaultVehicle()
+{
+    typeName = "Truck";
+    
+    ENSURE(getTypeName() == "Truck", "Truck constructor failed to set typeName");
+}
+
+Truck::Truck(RoadSystem* environment, const string& licensePlate, Road* currentRoad) :
+        DefaultVehicle::DefaultVehicle(environment, licensePlate, currentRoad, "Car", stdCarLength, &stdCarLimits)
+{
+}
+
+Truck::Truck(RoadSystem* environment, const string& licensePlate, Road* currentRoad, int acceleration,
+                       int speed, unsigned int position) :
+        DefaultVehicle::DefaultVehicle(environment, licensePlate, currentRoad, acceleration, speed, position, "Car", stdCarLength, &stdCarLimits)
+{
 }
--- src/TypedVehicles.h
+++ src/TypedVehicles.h
@@ -26,6 +26,28 @@ extern const VehicleLimits stdTruckLimits;
 
 ///--- Classes ---///
 
+class MotorCycle : public DefaultVehicle {
+public:
+    /**
+     * Default constructor
+     * @ENSURE NOT properly initialised, get typename = MotorCycle
+     */
+    MotorCycle();
+    
+    /**
+     * Minimal constructor
+     * Acceleration, speed and position are assumed to be 0
+     * @ENSURE properly initialised, get<arg> = <arg>, get typename = MotorCycle, get <acc/spd/pos> = 0, NOT update ready
+     */
+    MotorCycle(RoadSystem* environment, const string& licensePlate, Road* currentRoad);
+    
+    /**
+     * Maximal constructor
+     * @ENSURE properly initialised, get<arg> = <arg>, get typename = MotorCycle, NOT update ready
+     */
+    MotorCycle(RoadSystem* environment, const string& licensePlate, Road* currentRoad, int acceleration, int speed, unsigned int position);
+};
+
 class Car: public DefaultVehicle {
 public:
     /**
@@ -48,7 +70,60 @@ public:
     Car(RoadSystem* environment, const string& licensePlate, Road* currentRoad, int acceleration, int speed, unsigned int position);
 };
 
+class Bus: public DefaultVehicle {
+public:
+    /**
+     * Default constructor
+     * @ENSURE NOT properly initialised, get typename = Bus
+     */
+    Bus();
+    
+    /**
+     * Minimal constructor
+     * Acceleration, speed and position are assumed to be 0
+     * @ENSURE properly initialised, get<arg> = <arg>, get typename = Bus, get <acc/spd/pos> = 0, NOT update ready
+     */
+    Bus(RoadSystem* environment, const string& licensePlate, Road* currentRoad);
+    
+    /**
+     * Maximal constructor
+     * @ENSURE properly initialised, get<arg> = <arg>, get typename = Bus, NOT update ready
+     */
+    Bus(RoadSystem* environment, const string& licensePlate, Road* currentRoad, int acceleration, int speed, unsigned int position);
+    
+    /**
+     * Upate acceleration, speed and position (and possibly currentRoad)
+     * stops for bus stops
+     * @REQUIRE properly initialised, update prepared, simulation active
+     * @ENSURE get<acc/spd/pos> is within limits, minimum distance is respected, update NOT ready
+     */
+    virtual void execUpdate();
 
+private:
+    int busStopCooldown;
+};
+
+class Truck: public DefaultVehicle {
+public:
+    /**
+     * Default constructor
+     * @ENSURE NOT properly initialised, get typename = Truck
+     */
+    Truck();
+    
+    /**
+     * Minimal constructor
+     * Acceleration, speed and position are assumed to be 0
+     * @ENSURE properly initialised, get<arg> = <arg>, get typename = Truck, get <acc/spd/pos> = 0, NOT update ready
+     */
+    Truck(RoadSystem* environment, const string& licensePlate, Road* currentRoad);
+    
+    /**
+     * Maximal constructor
+     * @ENSURE properly initialised, get<arg> = <arg>, get typename = Truck, NOT update ready
+     */
+    Truck(RoadSystem* environment, const string& licensePlate, Road* currentRoad, int acceleration, int speed, unsigned int position);
+};
 
 
 #endif //LNJPSE_CAR_H
