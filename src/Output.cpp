f14e0f011d868d36d2f6097627a0d15fee815ed4
Joren Van Borm <Joren.VanBorm@student.uantwerpen.be>
2019-04-29 16:02:31 +0200

    extended text_graphic

--- src/Output.cpp
+++ src/Output.cpp
@@ -86,6 +86,8 @@ ostream& Output::textGraphicPrint(ostream& target, unsigned int maxChar) const
     }
     unsigned int metresPerChar = max((unsigned int) (longestD / maxChar), (unsigned int) 1);
     
+    target << "-~=| Scaling at " << metresPerChar << " metres / char |=~-" << endl;
+    
     // print each road
     for (unsigned long rdNum = 0; rdNum < rdVec.size(); ++rdNum)
     {
@@ -115,6 +117,11 @@ ostream& Output::textGraphicPrint(ostream& target, unsigned int maxChar) const
             currPos += metresPerChar;
         }
         while (currPos <= currRd->getLength());
+        
+        if (currRd->getConnection())
+        {
+            target << " ->" << currRd->getConnection()->getName();
+        }
         
         target << endl;
     }
